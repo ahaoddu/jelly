2e034d35904f07b743a67be0fbc70555ba2397dc
ahaoddu <ddu_cd@163.com>
2022-11-02 16:14:27 +0800

    init

---
+++ app/src/main/java/com/jelly/LauncherActivity.kt
@@ -0,0 +1,11 @@
+package com.jelly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LauncherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
