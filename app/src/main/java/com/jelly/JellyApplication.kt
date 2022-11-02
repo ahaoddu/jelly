95289f34973e20329718d776304be9c0f7567604
ahaoddu <ddu_cd@163.com>
2022-11-02 22:54:36 +0800

    主题框架搭建

---
+++ app/src/main/java/com/jelly/JellyApplication.kt
@@ -0,0 +1,10 @@
+package com.jelly
+
+import android.app.Application
+
+class JellyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- app/src/main/java/com/jelly/LauncherActivity.kt
+++ app/src/main/java/com/jelly/LauncherActivity.kt
@@ -2,10 +2,13 @@ package com.jelly
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.widget.AppCompatButton
 
 class LauncherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
